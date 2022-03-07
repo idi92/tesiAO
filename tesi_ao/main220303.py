22eba08447dec630c80008ea2dfe14ff8e1ee6a4
idi92 <100203312+idi92@users.noreply.github.com>
2022-03-07 17:52:45 +0100

    main220303 updated:

--- tesi_ao/main220303.py
+++ tesi_ao/main220303.py
@@ -16,17 +16,19 @@ def _what_I_do_on_terminal():  # don't use!
     # do and save WF maps for each scan sampling
     iea.do_more_scans('_v0')
     # load mcl objects into a list
-    mcls = iea.load_mcls('_v0')  # use _f0 for the default scan sampling list
-    iea.plot_all_interpolation_functions(mcls)
-    iea.plot_interpolation_error(mcls)
+    # use _f0 for the default scan sampling list
+    mcls_int = iea.load_mcls('_v0')
+    iea.plot_all_interpolation_functions(mcls_int)
+    iea.plot_interpolation_error(mcls_int)
     # from the 'old' mcls elements, we need the interpolated functions
     # to compute p2c and save the a 'new' measured mcl object
-    iea.do_calibrated_measure(mcls, '_v1')  # use _z1 for default
+    iea.do_calibrated_measure(mcls_int, '_v1')  # use _z1 for default
     # load new mcl
     mcls_meas = iea.load_calibrated_measure('_v1')
     # Plot the difference between the measured and expected deflection, as a
     # function of the expected one
-    iea.plot_Measured_vs_Expected(mcls_meas)
+    rms_list = iea.plot_Measured_vs_Expected(mcls_meas, mcls_int)
+    iea.fitting_Meas_vs_Exp(mcls_meas, rms_list, mcls_int)
 
 
 class InterpolationErrorAnalyzer():
@@ -79,6 +81,22 @@ class InterpolationErrorAnalyzer():
 
         plt.plot(f_int(span), span, '-', color=plt.gca().lines[-1].get_color())
 
+    def _get_common_deflection_range(self, mcl_list):
+        '''
+        Returns the extremes[a,b] of the common deflection domain
+        between all interpolated functions
+        Input: list, mcl_list
+        Returns: a, b
+        '''
+        min_container = []
+        max_container = []
+        for mcl in mcl_list:
+            min_container.append(min(mcl._deflection[0]))
+            max_container.append(max(mcl._deflection[0]))
+        a = max(min_container)
+        b = min(max_container)
+        return a, b
+
     def do_more_scans(self, version_file):
         '''
         For each scan sampling defined in NUM_SCAN_LIST, 
@@ -112,7 +130,7 @@ class InterpolationErrorAnalyzer():
         Plots all interpolated functions obtained by varying scan sampling,
         as a function of actuator's deflections.
         '''
-        plt.figure(101)
+        plt.figure(101 + self.ACTUATOR)
         plt.clf()
         plt.ion()
         plt.title('act#%d: interpolation functions for several scans' %
@@ -120,8 +138,8 @@ class InterpolationErrorAnalyzer():
         for mcl in mcl_list:
             self._plot_interpolation_function(mcl)
 
-        plt.xlabel('Commands [au]')
-        plt.ylabel('Deflection [m]')
+        plt.xlabel('Commands [au]', size=25)
+        plt.ylabel('Deflection [m]', size=25)
         plt.grid()
         plt.legend(loc='best')
 
@@ -134,27 +152,21 @@ class InterpolationErrorAnalyzer():
         '''
         Npt = 1024
 
-        min_container = []
-        max_container = []
         # looking for the common deflections domain for the interpolated
         # functions
-        for mcl in mcl_list:
-
-            min_container.append(min(mcl._deflection[0]))
-
-            max_container.append(max(mcl._deflection[0]))
+        min_span, max_span = self._get_common_deflection_range(mcl_list)
 
         common_span_deflections = np.linspace(
-            max(min_container), min(max_container), Npt)
+            min_span, max_span, Npt)
 
         # interpolated function with the biggest scans sampling
         f_ref = mcl_list[-1]._finter[0]
 
-        plt.figure(102)
+        plt.figure(102 + self.ACTUATOR)
         plt.clf()
         plt.ion()
         plt.title('act#%d:' % self.ACTUATOR +
-                  'cubic interpolation error w-r-t %dscans' % max(self.NUM_SCAN_LIST))
+                  'cubic interpolation error w-r-t %dscans' % max(self.NUM_SCAN_LIST), size=25)
 
         for idx, scans in enumerate(self.NUM_SCAN_LIST):
             f_i = mcl_list[idx]._finter[0]
@@ -163,8 +175,8 @@ class InterpolationErrorAnalyzer():
 
         plt.legend(loc='best')
         plt.grid()
-        plt.ylabel('Command Difference [au]')
-        plt.xlabel('Deflection [m]')
+        plt.ylabel('Command Difference [au]', size=25)
+        plt.xlabel('Deflection [m]', size=25)
 
     def do_calibrated_measure(self, mcl_list, version):
         '''
@@ -182,12 +194,15 @@ class InterpolationErrorAnalyzer():
 
         wyko, bmc = sandbox.create_devices()
 
-        expected_deflection = np.linspace(-800e-9, 1600e-9, Npt)
+        min_span, max_span = self._get_common_deflection_range(mcl_list)
+        expected_deflection = np.linspace(min_span, max_span, Npt)
+
+        # expected_deflection = np.linspace(-800e-9, 1600e-9, Npt) #@act63
 
         converted_cmd = np.zeros((len(mcl_list), Npt))
 
         for idx, mcl in enumerate(mcl_list):
-            mcm = MyCalibrationMeasurer(wyko, bmc, mcl)
+            mcm = MyCalibrationMeasurer(wyko, bmc, mcl, expected_deflection)
             mcm.execute_command_scan(act_list)
             fname = self.fpath + '%d' % Npt + 'meas' + version + \
                 '_cal%d' % self.NUM_SCAN_LIST[idx] + self.ffmt
@@ -209,35 +224,82 @@ class InterpolationErrorAnalyzer():
 
         return mcl_list
 
-    def plot_Measured_vs_Expected(self, mcl_meas):
+    def plot_Measured_vs_Expected(self, mcl_meas, mcl_int):
         '''
         Plots the difference between the measured and expected deflection,
         as a function of the expected one. 
         mcl_meas[i]== element of the list loaded from load_calibrated_measure
-        Input: list, mcls_meas 
+        Input: list, mcls_meas
+        list, mcl_int (used for common deflection domain evaluation)
         '''
         Npt = self.test_points
 
-        plt.figure(456)
+        plt.figure(456 + self.ACTUATOR)
         plt.clf()
-        x_exp = np.linspace(-800e-9, 1600e-9, Npt)  # expected deflections
+        min_span, max_span = self._get_common_deflection_range(mcl_int)
+        x_exp = np.linspace(min_span, max_span, Npt)  # expected deflections
+        rms_list = []
         for idx in np.arange(len(mcl_meas)):
 
             x_obs = mcl_meas[idx]._deflection[0]
             y = x_obs - x_exp
-
+            rms = y.std()
+            rms = rms / 1.e-9
+            rms_list.append(y.std())
             plt.plot(x_exp, y, 'o-', label='%d scans' %
                      self.NUM_SCAN_LIST[idx])
+            print('rms = %g' % rms + 'nm\t' +
+                  '(Sampling: %d scans)' % self. NUM_SCAN_LIST[idx])
 
         plt.legend(loc='best')
         plt.grid()
-        plt.xlabel('$x_{exp} [m]$')
-        plt.ylabel('$x_{obs} - x_{exp} [m]$')
+        plt.xlabel('$x_{exp} [m]$', size=25)
+        plt.ylabel('$x_{obs} - x_{exp} [m]$', size=25)
         plt.title('act#%d:' % self.ACTUATOR +
-                  ' Error in deflection cmds for each interpolation functions')
+                  ' Error in deflection cmds for each interpolation functions', size=25)
+        return rms_list
 
+    def fitting_Meas_vs_Exp(self, mcl_meas, rms_list, mcl_int):
+        '''
+        Plots the best fits for measured vs expected deflection, for each scan sampling.
+        '''
+        Npt = self.test_points
+
+        plt.figure(567 + self.ACTUATOR)
+        plt.clf()
+        min_span, max_span = self._get_common_deflection_range(mcl_int)
+        x_exp = np.linspace(min_span, max_span, Npt)
+        ones = np.ones(Npt)
+        xx = np.linspace(min_span, max_span, 1024)
+        for idx in np.arange(len(mcl_meas)):
+
+            x_obs = mcl_meas[idx]._deflection[0]
+
+            plt.plot(x_exp, x_obs, 'o', label='%d scans' %
+                     self.NUM_SCAN_LIST[idx])
+            sigma = ones * rms_list[idx]
+            coeff, coeff_cov = np.polyfit(
+                x_exp, x_obs, 1, w=sigma, cov=True, full=False)
+            err_coeff = np.diag(np.sqrt(coeff_cov))
+            print('Fit relative to Sampling: %d scans)' %
+                  self. NUM_SCAN_LIST[idx])
+            print('A = %g' % coeff[0] + '\t+/- %g ' % err_coeff[0])
+            print('offset = %g' % coeff[1] + '\t+/- %g' % err_coeff[1])
+            fit_func = np.poly1d(coeff)
+            plt.plot(xx, fit_func(xx), '-', label='relative fit',
+                     color=plt.gca().lines[-1].get_color())
+            # plt.errorbar(x_exp, x_obs, sigma,
+            #            color=plt.gca().lines[-1].get_color())
+        plt.legend(loc='best')
+        plt.grid()
+        plt.xlabel('$x_{exp} [m]$', size=25)
+        plt.ylabel('$x_{obs} [m]$', size=25)
+        plt.title('act#%d:' % self.ACTUATOR +
+                  ' Error in deflection cmds for each interpolation functions', size=25)
 
 # similar to CommandtoPositionLinearizationMeasurer
+
+
 class MyCalibrationMeasurer(object):  # changes when bmc set shape
     '''
     As CommandToPositionLinearizationMeasurer defined in sandbox.py, 
@@ -248,11 +310,12 @@ class MyCalibrationMeasurer(object):  # changes when bmc set shape
     NUMBER_WAVEFRONTS_TO_AVERAGE = 1
     NUMBER_STEPS_VOLTAGE_SCAN = 10
 
-    def __init__(self, interferometer, mems_deformable_mirror, mlc):
+    def __init__(self, interferometer, mems_deformable_mirror, mlc, expected_deflections):
         self._interf = interferometer
         self._bmc = mems_deformable_mirror
         self._n_acts = self._bmc.get_number_of_actuators()
         self._mlc = mlc
+        self._exp_deflections = expected_deflections
         self._wfflat = None
 
     def _get_zero_command_wavefront(self):
@@ -282,11 +345,10 @@ class MyCalibrationMeasurer(object):  # changes when bmc set shape
             (n_acts_to_meas, self.NUMBER_STEPS_VOLTAGE_SCAN,
              wfflat.shape[0], wfflat.shape[1]))
 
-        expected_deflection = np.linspace(-800e-9,
-                                          1600e-9, self.NUMBER_STEPS_VOLTAGE_SCAN)
         for act_idx, act in enumerate(self._actuators_list):
 
-            self._cmd_vector[act_idx] = self._mlc.p2c(act, expected_deflection)
+            self._cmd_vector[act_idx] = self._mlc.p2c(
+                act, self._exp_deflections)
             for cmd_idx, cmdi in enumerate(self._cmd_vector[act_idx]):
                 print("Act:%d - command %g" % (act, cmdi))
                 cmd = np.zeros(self._n_acts)
